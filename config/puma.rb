d23e80f63006a5c90aed61539f67daf7778b384e
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2020-06-30 10:05:21 -0300

    update bin stubs

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env puma
+# frozen_string_literal: true
 
 workers Integer(ENV['WEB_CONCURRENCY'] || 1)
 min_threads = Integer(ENV['MIN_THREADS'] || 16)
@@ -7,4 +8,4 @@ threads min_threads, max_threads
 
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'production'
+environment ENV['RACK_ENV'] || 'development'
